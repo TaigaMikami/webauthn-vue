ce6716c3fb36a7a2129f49850c274f47326d40ea
TaigaMikami <mikamimikami60@gmail.com>
2022-05-06 14:09:10 +0900

    add authentication page

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "@/pages/home.vue";
+import Authentication from "@/pages/authentication.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/authentication",
+    name: "Authentication",
+    component: Authentication
   }
 ];
 
